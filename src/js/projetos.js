a9072a4401b7780b39a9d894ebdea4bac7d17279
Eduardo Lobo <103695641+EdduLobo@users.noreply.github.com>
2023-07-27 06:37:23 -0300

    final_update

---
+++ src/js/projetos.js
@@ -0,0 +1,81 @@
+/* alternar color navbar */
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  const headerHeight = header.offsetHeight;
+  const scrolled = window.scrollY;
+
+  if (scrolled > headerHeight) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/* Scroll section */
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector('header nav a[href*="' + id + '"]')
+          .classList.add("active");
+      });
+    }
+  });
+  /* Sticky Navbar */
+
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /* remover alternar icon e navbar quando link da navbar for clicado  */
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+/* scroll reveal */
+ScrollReveal({
+  reset: false,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skills-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/* typed js */
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Nail Desinger", "Bem-vinda a minha página!", "Siga-me no Insta!"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
